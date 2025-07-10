7ee2138c1f26cc425e43c86cb9243580106435ca
Muhammad Fadhilah <muhammadfadhilah443@gmail.com>
2025-07-10 20:19:30 +0700

    feat: adjusted the width of the main container and buttons

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -5,9 +5,9 @@ import { IconBrandGoogleFilled, IconBrandGithub } from "@tabler/icons-react";
 
 export default function Page() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-3.5">
+    <div className="m-auto flex h-screen w-full max-w-[300px] flex-col items-center justify-center space-y-3.5">
       <h1 className="text-2xl font-bold">Create an account</h1>
-      <form className="space-y-2.5">
+      <form className="w-full space-y-2.5">
         <Input placeholder="Name" name="name" required />
         <Input placeholder="Email address" type="email" name="email" required />
         <Input
@@ -16,7 +16,7 @@ export default function Page() {
           name="password"
           required
         />
-        <Button className="w-[300px]">Register</Button>
+        <Button className="w-full">Register</Button>
       </form>
       <p className="text-gray-600">
         Already have an account?{" "}
@@ -27,14 +27,14 @@ export default function Page() {
       <p className="text-center text-gray-600">or</p>
       <Button
         variant="secondary"
-        className="w-[300px] font-semibold hover:bg-gray-300"
+        className="w-full font-semibold hover:bg-gray-300"
       >
         <IconBrandGoogleFilled />
         Continue with Google
       </Button>
       <Button
         variant="secondary"
-        className="w-[300px] font-semibold hover:bg-gray-300"
+        className="w-full font-semibold hover:bg-gray-300"
       >
         <IconBrandGithub />
         Continue with GitHub
