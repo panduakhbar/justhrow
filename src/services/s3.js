91eb1cdd913524d74bf378657c23724423c77114
Muhammad Fadhilah <muhammadfadhilah443@gmail.com>
2025-07-16 14:56:33 +0700

    feat: add presigned url utility

--- src/services/s3.js
+++ src/services/s3.js
@@ -1,6 +1,7 @@
 import { CLOUDFLARE_R2_BUCKET } from "@/lib/constant";
 import { r2Client } from "@/lib/s3";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { createId } from "@paralleldrive/cuid2";
 
 export async function upload({ file, folder }) {
@@ -30,7 +31,37 @@ export async function uploadMany({ files, folder }) {
   return await Promise.all(files.map((file) => upload({ file, folder })));
 }
 
-export async function getPresignedUrl({ path, expiresIn = 3600 }) {
-  // should return url
-  return "";
+export async function getPresignedUrl({ path, contentType, expiresIn = 300 }) {
+  try {
+    const command = new PutObjectCommand({
+      Bucket: CLOUDFLARE_R2_BUCKET,
+      Key: path,
+      ContentType: contentType,
+    });
+
+    const presignedUrl = await getSignedUrl(r2Client, command, {
+      expiresIn,
+    });
+
+    return presignedUrl;
+  } catch (error) {
+    console.log("[S3] Failed to get Presigned URL: ", error);
+    if (
+      error.name === "CredentialsProviderError" ||
+      error.name === "AccessDenied" ||
+      error.name === "InvalidAccessKeyIdError" ||
+      error.name === "SignatureDoesNotMatchError"
+    ) {
+      throw new Error("Authentication failed with R2 service.");
+    } else if (
+      error.code === "ECONNREFUSED" ||
+      error.code === "ENOTFOUND" ||
+      error.code === "ETIMEDOUT" ||
+      error.name === "NetworkError"
+    ) {
+      throw new Error("Network connection failed.");
+    } else {
+      throw new Error(`Failed to generate presigned URL: ${error.message}`);
+    }
+  }
 }
