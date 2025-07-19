f74010dcaac2b0778b117b0ced743f9d55986ee4
afifudin-f <afif.abdullah@feedloop.ai>
2025-07-19 22:03:22 +0700

    switch to prisma

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,6 @@
 "server-only";
 
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient().$extends(withAccelerate());
